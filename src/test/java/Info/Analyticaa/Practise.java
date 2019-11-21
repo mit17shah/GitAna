d174ddc7f6fc20c15f1e736209c636273e1033e2
Mitesh <mandm1507@gmail.com>
2019-11-22 01:24:15 +0530

    Second Commit

---
+++ src/test/java/Info/Analyticaa/Practise.java
@@ -0,0 +1,14 @@
+package Info.Analyticaa;
+
+import org.testng.annotations.Test;
+
+public class Practise {
+	
+	
+	@Test
+	public void practise1()
+	{
+		System.out.println("I am Practise1");
+	}
+
+}
