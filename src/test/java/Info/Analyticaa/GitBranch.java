71648249f523b3a8cf2a4c385147392b9cb24f91
Mitesh <mandm1507@gmail.com>
2019-11-22 01:39:12 +0530

    Branch new

---
+++ src/test/java/Info/Analyticaa/GitBranch.java
@@ -0,0 +1,16 @@
+package Info.Analyticaa;
+
+import org.testng.annotations.Test;
+
+public class GitBranch {
+	
+	@Test
+	public void Branch()
+	{
+		
+		System.out.println("Branch 1");
+		System.out.println("Branch 1");
+		
+	}
+
+}
