4ece8d98b5175f1a031298a017ccf290557b9874
Mitesh <mandm1507@gmail.com>
2019-11-22 01:27:44 +0530

    Third Commit

--- src/test/java/Info/Analyticaa/Practise.java
+++ src/test/java/Info/Analyticaa/Practise.java
@@ -9,6 +9,9 @@ public class Practise {
 	public void practise1()
 	{
 		System.out.println("I am Practise1");
+		System.out.println("I am Practise2");
+		System.out.println("I am Practise3");
+		System.out.println("I am Practise4");
 	}
 
 }
