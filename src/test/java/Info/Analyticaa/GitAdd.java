eaf1eed38c8f4c484211b67211d5a15aa3263498
Mitesh <mandm1507@gmail.com>
2019-11-22 01:32:28 +0530

    Fourth Commit

---
+++ src/test/java/Info/Analyticaa/GitAdd.java
@@ -0,0 +1,18 @@
+package Info.Analyticaa;
+
+import org.testng.annotations.Test;
+
+public class GitAdd {
+
+	
+	@Test
+	public void Test1()
+	{
+		System.out.println("I am Test1");
+		System.out.println("I am Test2");
+		System.out.println("I am Test3");
+		System.out.println("I am Test4");
+		System.out.println("I am Test5");
+		
+	}
+}
